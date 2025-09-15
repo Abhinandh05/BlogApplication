ad43efd6458f37298712377488c01375c255bab6
Abhinandhc <abhinandhc407@gmail.com>
2025-09-15 17:16:23 +0530

    Completed the userRoute

---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {getUserProfile, login, logout, register} from "../controllers/user.controller.js";
+import {isAuthenticated} from "../middleware/ isAuthenticated.js";
+
+
+export const router = express.Router();
+
+router.route('/register').post(register);
+router.route('/login').post( login);
+router.route('/profile').post(isAuthenticated, getUserProfile);
+router.route('/logout').post(logout);
+
+
