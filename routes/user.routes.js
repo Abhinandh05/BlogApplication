7f51a37f8514fbdf3cbe38c2d0557730756b2be4
Abhinandhc <abhinandhc407@gmail.com>
2025-09-16 10:26:53 +0530

    completed the admin seed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { getUserProfile, login, logout, register } from "../controllers/user.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import {seedAdmin} from "../seeds/admin.seed.js";
 
 const router = express.Router();
 
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/admin", seedAdmin)
 
 
 router.get("/profile", isAuthenticated, getUserProfile);
---
+++ seeds/admin.seed.js
@@ -0,0 +1,41 @@
+import {User} from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
+export const seedAdmin = async (req, res) =>{
+     try{
+         // checking the admin is already existing or not
+
+         const existingAdmin = await User.findOne({role:"admin"})
+
+         if (existingAdmin){
+             return res.status(409).json({
+                 message:"admin already existing ",
+                 email:existingAdmin.email
+             })
+         }
+
+         const hashedPassed = await bcrypt.hash("admin@123" ,10 )
+
+         const admin = await User.create({
+             fullName:"Main Admin",
+             email:"abhinandhc407@gmail.com",
+             password:hashedPassed,
+             phoneNumber:89098787,
+             role:"admin"
+         });
+
+         return res.status(201).json({
+             message:"Admin logged successfully",
+             email: admin.email,
+         })
+
+     } catch (err){
+         console.log("Something went to wrong ")
+
+         return res.status(400).json({
+             message:"Something went to wrong while creating the admin seed ",
+             success: false
+         })
+     }
+}
