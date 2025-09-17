dc78c1b1fd350b7b506c2b1586bd92a09c15c1c1
Abhinandhc <abhinandhc407@gmail.com>
2025-09-17 10:23:58 +0530

    add complted the user admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,4 +1,5 @@
 import {User} from "../models/user.model.js";
+import {Post} from "../models/post.model.js";
 
 
 export const getAllUser = async (req, res) =>{
@@ -58,6 +59,90 @@ export const deleteUser = async (res, req) =>{
         const user = await User.findByIdAndDelete(id)
 
     } catch (err){
+        console.log("Something went to wrong ")
+        return res.status(500).json({
+            message:"Internal server error",
+            success: false
+        })
+    }
+}
+
+// admin can handle the post
+
+export const getAllPost = async (req,  res) =>{
+    try{
+
+        const posts = await Post.find().populate('author', 'fullName')
+        if (!posts){
+            return res.status(400).json({
+                message:"Post not found ",
+                success:false
+
+            })
+        }
+      return res.status(200).json({
+          message:"Get all the post ",
+          success: true,
+          posts
+      })
+
+    }catch (err){
+        console.log("Something went to wrong ")
+        return res.status(500).json({
+            message:"Internal server error",
+            success: false
+        })
+    }
+}
+
+export const updatePost = async (req, res) =>{
+    try{
+
+        const {id} = req.params;
+        const updates = req.body;
+
+        const post = await Post.findByIdAndUpdate(id, updates,{new: true});
+
+        if (!post){
+            return res.status(400).json({
+                message:"Post not found",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            message:"Post updated successfully",
+            success: false,
+            post
+        })
+
+    } catch (err){
+        console.log("Something went to wrong ")
+        return res.status(500).json({
+            message:"Internal server error",
+            success: false
+        })
+    }
+}
+
+export const deletePost = async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const post = await Post.findByIdAndDelete(id)
+
+        if (!post){
+            return res.status(400).json({
+                message:"can not delete the post ",
+                success: false
+            })
+        }
+
+        return res.status(200).json({
+            message:"The post deleted successfully",
+            success:true,
+            post
+        })
+
+    }  catch (err){
         console.log("Something went to wrong ")
         return res.status(500).json({
             message:"Internal server error",
